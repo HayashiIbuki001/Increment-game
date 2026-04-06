1aab4a38cee461dfc2f1afcb37fc43f7b15ed2f5
HayashiIbuki001 <sch-w4meleer@outlook.jp>
2026-04-07 02:47:57 +0900

    [追加]ごみをドラッグできるようにした

---
+++ Assets/Scripts/Trash.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class Trash : MonoBehaviour
+{
+    private void OnMouseDrag()
+    {
+        //マウスカーソル及びオブジェクトのスクリーン座標を取得
+        Vector3 objectScreenPoint =
+            new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10);
+
+        //スクリーン座標をワールド座標に変換
+        Vector3 objectWorldPoint = Camera.main.ScreenToWorldPoint(objectScreenPoint);
+
+        //オブジェクトの座標を変更する
+        transform.position = objectWorldPoint;
+    }
+}
